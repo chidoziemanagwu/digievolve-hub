ef91e960f22d7fff767f2025a4fec9bcb35546a9
Mr. Managwu <39161025+chidoziemanagwu@users.noreply.github.com>
2025-02-24 13:53:49 +0000

    .

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,4 @@
-import path from 'path';
+import path from 'path/posix';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -19,7 +19,7 @@ const nextConfig = {
 
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': path.resolve(__dirname, 'src'),
+      '@': path.resolve(process.cwd(), 'src'),
     };
 
     return config;
